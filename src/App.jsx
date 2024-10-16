e41a3a619b3c846170ba91259a3390ed145a2d81
Sana Rehmat <sanarehmatali7@gmail.com>
2024-10-16 10:56:16 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'; // Use index.css instead of App.css
+
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-blue-500 flex justify-center items-center">
+      <h1 className="text-4xl text-white">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
